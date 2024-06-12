68207cf90a0cc087011bb8a6c8c7051927ef9e06
brunofleitas <brunofleitas2011@gmail.com>
2024-06-13 01:07:39 +0200

    changes to test colors

--- 5-rendering_mandelbrot.c
+++ 5-rendering_mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: bfleitas <bfleitas@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 01:38:58 by bfleitas          #+#    #+#             */
-/*   Updated: 2024/06/12 03:29:51 by bfleitas         ###   ########.fr       */
+/*   Updated: 2024/06/13 01:07:02 by bfleitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void    put_pixel_buffer(t_fractal *fractal, int x, int y, int color)
 {
-    char    *dst;
+    int    *dst;
 
-    dst = fractal->image.address + (y * fractal->image.line_length + x * (fractal->image.bits_per_pixel / 8));
-    *(unsigned int *)dst = color;
+    dst = (y * fractal->image.line_length + x * (fractal->image.bits_per_pixel / 8));
+    *(unsigned int *)(img->pixels_ptr + dst) = color;
 } 
 
 void    put_in_buffer_mandelbrot(int x, int y, t_fractal *fractal)
@@ -25,21 +25,25 @@ void    put_in_buffer_mandelbrot(int x, int y, t_fractal *fractal)
     t_complex   c;
     t_complex   z;
     int         i;
+    int        color;
 
     z.real = 0;
     z.imaginary = 0;
     i = 0;
     c.real = scale(x, -2, 2, WIDTH);
     c.imaginary = scale(y, 2, -2, HEIGHT);
-    while (i < fractal->max_iter && (z.real * z.real + z.imaginary * z.imaginary) < fractal->scape_radius)
+    while (i < fractal->max_iter )
     {
         z = sum(square(z), c);
+        if ((z.real * z.real + z.imaginary * z.imaginary) < fractal->escape_value)
+		{
+			color = map(i, 0xFFFFFF, 0X000000, 0, fractal->max_iter);
+			my_pixel_put(x, y, &fractal->img, color);
+			return ;
+		}
         i++;
     }
-    if (i == fractal->max_iter)
-        put_pixel_buffer(fractal, x, y, 0x000000);
-    else
-        put_pixel_buffer(fractal, x, y, 0xFFFFFF);
+    my_pixel_put(x, y, &fractal->img, 0x000000);
 }
 
 void   rendering_mandelbrot(t_fractal *fractal)
