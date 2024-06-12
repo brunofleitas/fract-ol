0b6ae6e5761e5175451c9a7da33ab31130c4f68f
brunofleitas <brunofleitas2011@gmail.com>
2024-06-12 02:16:23 +0200

    fix problem

--- rendering_mandelbrot.c
+++ rendering_mandelbrot.c
@@ -6,13 +6,13 @@
 /*   By: bfleitas <bfleitas@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 01:38:58 by bfleitas          #+#    #+#             */
-/*   Updated: 2024/06/12 02:02:32 by bfleitas         ###   ########.fr       */
+/*   Updated: 2024/06/12 02:16:03 by bfleitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-double scale(double input, double old_max, double new_min, double new_max)
+double scale(double input, double new_min, double new_max, double old_max)
 {
     return (new_min + (new_max - new_min) * ((input) / (old_max)));
 }
@@ -26,8 +26,8 @@ void    put_in_buffer(int x, int y, t_fractal *fractal)
     z.real = 0;
     z.imaginary = 0;
     i = 0;
-    c.real = scale(x, WIDTH, fractal->min.real, fractal->max.real);
-    c.imaginary = scale(y, HEIGHT, fractal->min.imaginary, fractal->max.imaginary);
+    c.real = scale(x, -2, 2, WIDTH);
+    c.imaginary = scale(y, 2, -2, HEIGHT);
     while (i < fractal->max_iter && (z.real * z.real + z.imaginary * z.imaginary) < escape_radius)
     {
         z = sum(square(z), c);
